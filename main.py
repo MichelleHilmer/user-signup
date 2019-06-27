288a33009c7c86a0ef3ecb28a38055f8a741c1d8
Michelle Hilmer <mblanke1988@gmail.com>
2019-06-27 17:40:26 -0500

    got some things working

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -8,31 +8,28 @@ def welcome():
     user_name = request.form["user_name"]
     return render_template("welcome.html")
 
-@app.route("/email", methods=["GET"])
-def email():
-    return render_template("user_name.html")
 
-@app.route("/var_pass", methods=["POST"])
-def varify_password():
-    return render_template("user_name.html")
+@app.route("/user", methods=["GET", "POST"])
+def user_info():
 
+        user_name = request.form["user_name"]
+        password = request.form["password"]
+        varify_password = request.form["varify_password"]
+        email = request.form["email_optional"]
 
-@app.route("/password", methods=["POST"])
-def password():
-    return render_template("user_name.html")
+        #if ' ' in user_name:
+                #error = "Please enter valid user name."
+                #return redirect("/?error=" + error)
 
+        #if user_name.length < 3 or user_name.length > 20:
+                #error = "Please make user_name between 3 and 20 characters."
+    
 
-
-@app.route("/user", methods=['GET'])
-def user_name():
-    user_name = request.form['user_name']
-
-    if ' ' in user_name:
-        error = "Please enter valid e-mail."
+        return render_template("welcome.html", user_name=user_name)
 
 @app.route ('/')
 def index():
-    return render_template('user_name.html')
+    return render_template("user_name.html")
 
 app.run()
 
