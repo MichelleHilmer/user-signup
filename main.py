4c405ba8a64244550e84768f249c8803798c0cac
Michelle Hilmer <mblanke1988@gmail.com>
2019-07-01 11:41:08 -0500

    omg thank god done

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def user_info():
 
         user_name = request.form['user_name']
         password = request.form['password']
-        varify_password = request.form['password_verify']
+        password_verify = request.form['password_verify']
         email = request.form['email']
 
         
@@ -31,19 +31,67 @@ def user_info():
 
         user_name_error = ''
         password_error = ''
-        varify_password_error = ''
+        verify_password_error = ''
         email_error = ''
 
         if len(user_name) <= 0:
                 user_name_error = 'Please enter a valid user name.'
                 user_name = ''
 
-        if not user_name_error:
+        if len(password) <=0:
+                password_error = 'Please enter a valid password.'
+                password = ''
+        
+        if len(password_verify) <=0:
+                verify_password_error = 'Please enter a valid password.'
+                password_verify = ''
+
+        if len(user_name) < 3 or len(user_name) > 20:
+                user_name_error = "Please use a user name between 3 and 20 letters."
+                user_name = ''
+
+        if len(password) < 3 or len(password) >20:
+                password_error = "Please enter a password between 3 and 20 characters."
+                password = ''
+
+        if len(password_verify) < 3 or len(password_verify) >20:
+                verify_password_error = "Please enter a password between 3 and 20 characters."
+                password_verify = ''
+
+        if password_verify != password:
+                verify_password_error = "Passwords dont match!"
+                password = ''
+                password_verify = ''
+
+        if ' ' in user_name:
+                user_name_error = "Please enter a valid user name."
+                user_name = ''
+        
+        if ' ' in password:
+                password_error = 'Please enter a valid password'
+                password = ''
+
+        if ' ' in password_verify:
+                verify_password_error = "Please enter a valid password."
+                password_verify = ''
+
+        
+        if len(email) > 0:
+                if '@' not in email and '.' not in email or ' ' in email:
+                        email_error = 'Thats not a valid email.'
+                        email = ''
+                elif len(email) < 3 and len(email) > 20:
+                        email_error = "Thats not a valid email."
+                        email = ''
+
+
+        if not user_name_error and not password_error and not verify_password_error and not email_error:
                 template = jinja_env.get_template('welcome.html')
                 return template.render(user_name=user_name)
         else:
                 template = jinja_env.get_template('user_name.html')
-                return template.render('user_name.html', user_name_error=user_name_error, user_name=user_name)
+                return template.render(user_name_error=user_name_error, user_name=user_name, password=password, password_verify=password_verify, email=email,
+                password_error=password_error, verify_password_error=verify_password_error, email_error=email_error)
 
 @app.route ('/')
 def index():
